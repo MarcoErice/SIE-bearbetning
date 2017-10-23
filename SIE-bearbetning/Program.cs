cf5a0f95778004b1198e67b627189cacadeef227
MarcoErice <marco.erice@gmail.com>
2017-10-23 13:06:39 +0200

    Updates done.

--- SIE-bearbetning/Program.cs
+++ SIE-bearbetning/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SIE_bearbetning
@@ -10,6 +13,59 @@ namespace SIE_bearbetning
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hej, var god ange sökvägen till en SIE-fil");
+            var theFile = Console.ReadLine();
+            string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+
+            int counter = 0;
+            string content = File.ReadAllText(theFile);
+            var reader = new StringReader(content);
+            
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    if (Regex.Match(line, pattern).Success)
+                    {
+                        counter++;
+                    }
+                }
+
+            var accounts = new Dictionary<string, decimal>();
+            reader = new StringReader(content);
+            
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                    break;
+                // string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    var accountId = match.Groups[1].Value;
+                    var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+                    if (accounts.ContainsKey(accountId))
+                    {
+                        accounts[accountId] += amount;
+                    }
+                    else
+                        accounts[accountId] = amount;
+                }
+
+            }
+
+
+            Console.WriteLine($"Antal #TRANS som finns i filen är: {counter}");
+            foreach (var entry in accounts.OrderBy(e => e.Key))
+            {
+                Console.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+            }
+            Console.WriteLine("Summan av alla konton är:");
+            Console.WriteLine(accounts.Sum(entry => entry.Value));
+            Console.ReadLine();
         }
     }
 }
