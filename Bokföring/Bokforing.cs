bb55b261a3bea9b8a29085aca6737f1c97918ab7
MarcoErice <marco.erice@gmail.com>
2017-10-24 12:15:27 +0200

    Updates done.

--- Bokföring/Bokforing.cs
+++ Bokföring/Bokforing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,8 +15,8 @@ namespace Bokföring
         {
             Console.WriteLine("Please add a path to the SIE-file:");
             var theFile = Console.ReadLine();  //  C:\Users\marco\Desktop\SIE.txt
-            string pattern = @"(#TRANS)";
-
+            string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+                        
             var counter = 0;
             var content = File.ReadAllText(theFile);
             var reader = new StringReader(content);
@@ -29,9 +30,42 @@ namespace Bokföring
                         counter++;
                     }
                 }
-            
 
-            Console.WriteLine(counter);
+            var TransAccounts = new Dictionary<string, decimal>();
+            var streamReader = File.OpenText(theFile);
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+                
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    var accountId = match.Groups[1].Value;
+                    var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+                    if (TransAccounts.ContainsKey(accountId))
+                    {
+                        TransAccounts[accountId] += amount;
+                    }
+                    else
+                        TransAccounts[accountId] = amount;
+                }
+                
+
+            }
+
+            Console.WriteLine($"Amount #TRANS accounts in the file are {counter}.");
+            Console.WriteLine();
+            foreach (var entry in TransAccounts.OrderBy(e => e.Key))
+            {
+                Console.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Summan av alla konton är:");
+            Console.WriteLine(TransAccounts.Sum(e => e.Value));
             Console.ReadLine();
         }
     }
